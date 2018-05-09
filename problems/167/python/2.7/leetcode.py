5f68c03dc758e961168c3ca955c196a62ab324f2
yqtang <yqtang1222@gmail.com>
2018-05-09 22:55:33 +0800

    create problem 167 and solution

---
+++ problems/167/python/2.7/leetcode.py
@@ -0,0 +1,32 @@
+class Solution(object):
+        
+    def twoSum(self, nums, target):
+        """
+        Use dict to store data(fastest)
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        diff_records = {}
+        for k, v in enumerate(nums):
+            d = target - v # Hint 1
+            if d in diff_records:
+                return [diff_records[d]+1, k+1]
+            diff_records[v] = k # save v and k for next round
+               
+
+    def test(self):
+        """
+            Given nums = [2, 7, 11, 15], target = 9,
+
+            Because nums[0] + nums[1] = 2 + 7 = 9,
+            return [1, 2].
+        """
+        target = 9
+        test = [2, 7, 11, 15]
+        result = self.twoSum(test, target)
+        print(result)
+        
+sol = Solution()
+sol.test()
