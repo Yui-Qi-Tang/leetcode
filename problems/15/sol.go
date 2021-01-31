7da68dc3fe3a5d30ad12ce4f36e6bed954945c38
Yuki Tang <yqtang1222@gmail.com>
2021-02-01 00:45:54 +0800

    finished problem 15

--- problems/15/sol.go
+++ problems/15/sol.go
@@ -1,24 +1,39 @@
 package sol
 
-import "fmt"
+import "sort"
 
 func threeSum(nums []int) [][]int {
-
-	diff := make(map[int]int)
-	result := make([][]int, 0)
-
-	if len(nums) == 0 {
-		return nil
+	var result [][]int
+	if len(nums) < 3 {
+		return result
 	}
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-2; i++ {
+		if i != 0 && nums[i] == nums[i-1] {
+			continue // avoid to duplicated computing
+		}
+		target := nums[i]
+		// 2 pointers
+		low := i + 1
+		high := len(nums) - 1
+		for low < high {
+			sum := target + nums[low] + nums[high]
+			if sum == 0 { // satify condition
+				result = append(result, []int{target, nums[low], nums[high]}) // save result
 
-	for _, n := range nums {
-		_, ok := diff[n]
-		if !ok {
-			diff[n] = 0 - n
+				// try to find another pair to satisfy the target
+				for low++; low < high && nums[low] == nums[low-1]; {
+					low++ //skip the duplicated value
+				}
+				for high--; low < high && nums[high] == nums[high+1]; {
+					high-- //skip the duplicated value
+				}
+			} else if sum < 0 {
+				low++ // find the small one (HINT: nums is sorted)
+			} else {
+				high-- // find the bigger one (HINT: nums is sorted)
+			}
 		}
 	}
-
-	fmt.Println(diff)
-
 	return result
 }
--- problems/15/sol_test.go
+++ problems/15/sol_test.go
@@ -3,5 +3,36 @@ package sol
 import "testing"
 
 func TestBasic(t *testing.T) {
-	threeSum([]int{-1, 0, 1, 2, -1, -4})
+
+	testcases := []struct {
+		input []int
+		want  [][]int
+	}{
+		{
+			input: []int{-1, 0, 1, 2, -1, -4},
+			want: [][]int{
+				{-1, -1, 2},
+				{-1, 0, 1},
+			},
+		},
+	}
+
+	for _, testcase := range testcases {
+		result := threeSum(testcase.input)
+		if len(result) != len(testcase.want) {
+			t.Fatal("expected length is:", len(testcase.want), "but got:", len(result))
+		}
+
+		for i, want := range testcase.want {
+			res := result[i]
+
+			for j, w := range want {
+				if res[j] != w {
+					t.Fatal("expected:", testcase.want, "but got:", result)
+				}
+			}
+		}
+	}
+
+	t.Log("passed...")
 }
